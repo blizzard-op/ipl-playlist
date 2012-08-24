0bf2475192b117d7713eeb77cc4a66514e2e5ec9
Bennett <bennett@howdycloud.com>
2012-08-24 14:31:59 -0700

    parse franchise and startsAt

--- main.go
+++ main.go
@@ -3,21 +3,34 @@ package main
 import (
 	"fmt"
 	"flag"
+	"time"
 )
 
+const timeFormat = time.RFC3339
+
+// franchise
 var franchiseName string;
 
-func init() {
-	fmt.Printf("Initializing...\n")
-	flag.StringVar(&franchiseName, "franchise", "StarCraft 2", "name of franchise")
-}
+// start time
+var startsAt time.Time;
+var startsAtTime string;
 
 func main() {
-	flag.Parse() // parses the logging flags
+	flag.StringVar(&franchiseName, "franchise", "StarCraft 2", "Name of franchise. Default is StarCraf 2.")
+	flag.StringVar(&startsAtTime, "startsAt", time.Now().Format(timeFormat), "Start time. Default is now.")
+	flag.Parse() // parses the flags
 
-	fmt.Printf("Starting...\n")
+	startsAt, err := time.Parse(timeFormat, startsAtTime)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }	
 
-	fmt.Printf("n %s\n", franchiseName)
+	fmt.Printf("\n* Starting...\n")
+
+	fmt.Printf("Franchise\n\targ: %s\n", franchiseName)
+
+	fmt.Printf("Starts\n\targ: %v\n\ttime: %v\n", startsAtTime, startsAt)
 
 	fmt.Printf("Done.\n")
 }	
