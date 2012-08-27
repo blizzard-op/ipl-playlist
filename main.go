51c0affdef7f3bb10fea201204c4cb32865d198b
Bennett <bennett@howdycloud.com>
2012-08-27 13:13:32 -0700

    parse config filepath

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"time"
 	playlist "./playlist"
+	//yaml "github.com/kylelemons/go-gypsy/yaml"
 )
 
 const timeFormat = time.RFC3339
@@ -20,11 +21,15 @@ var startsAtTime string;
 var endsAt time.Time;
 var endsAtTime string;
 
+// config filepath
+var configFilepath string;
+
 func init() {
 	now := time.Now()
-	flag.StringVar(&franchiseName, "franchise", "StarCraft 2", "Name of franchise. Default is StarCraf 2.")
+	flag.StringVar(&franchiseName, "franchise", "StarCraft 2", "Name of franchise. Default is StarCraft 2.")
 	flag.StringVar(&startsAtTime, "start", now.Format(timeFormat), "Start time. Default is now.")
 	flag.StringVar(&endsAtTime, "end", now.Add(time.Hour * 24).Format(timeFormat), "End time. Default is 24 hours from now.")
+	flag.StringVar(&configFilepath, "c", "config.yml", "Config filepath. Default is './config.yml.'")
 	flag.Parse() // parses the flags
 	parseTimeVar(timeFormat, startsAtTime, &startsAt) // parse startsAt
 	parseTimeVar(timeFormat, endsAtTime, &endsAt) // parse endsAt
@@ -46,6 +51,7 @@ func main() {
 	fmt.Printf("Franchise\n\targ: %s\n", franchiseName)
 	fmt.Printf("Starts\n\targ: %v\n\ttime: %v\n", startsAtTime, startsAt)
 	fmt.Printf("Ends\n\targ: %v\n\ttime: %v\n", endsAtTime, endsAt)
+	fmt.Printf("Config\n\targ: %s\n", configFilepath)
 
 	// construct playlist
 	playlist := playlist.Playlist{startsAt, endsAt}
