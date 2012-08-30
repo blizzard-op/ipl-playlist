ba65ac6eaccf39ffd0b5a2374e45bd3245f44034
Bennett <bennett@howdycloud.com>
2012-08-30 15:34:23 -0700

    refactor config to class

---
+++ playlist/config.go
@@ -0,0 +1,44 @@
+package playlist
+
+import (
+	"strconv"
+	"errors"
+	yaml "github.com/kylelemons/go-gypsy/yaml"
+)
+
+func validateItems(config yaml.File, key string) (yaml.List, error) {
+	node, err := yaml.Child( config.Root, key )
+	if err != nil {
+		return nil, err
+	}
+	lst, ok := node.(yaml.List)
+	if (!ok || (lst.Len() <= 0)) {
+		return nil, errors.New("Invalid items")
+	}
+	return lst, nil
+}
+
+func getItems(config yaml.File, key string) ([]*PlaylistBlock, error){
+	lst, err := validateItems(config, key)
+	if( err != nil){
+		return nil, errors.New("Invalid items")
+	}
+	items := make([]*PlaylistBlock, lst.Len())
+	for i, e := range lst {
+		itemKey := key + "[" + strconv.Itoa(i) + "]"
+		title, err := config.Get(itemKey + ".title")
+		if (err != nil) {
+			return nil, errors.New("Missing title")
+		}
+		series, err := config.Get(itemKey + ".series")
+		if (err != nil) {
+			series = ""
+		}
+		filepathsNode, err := yaml.Child( e, "filepaths" )
+		if err != nil {
+			return nil, errors.New("Missing filepaths for " + title)
+		}
+		items[i] = new(PlaylistBlock).Init(title, series, filepathsNode.(yaml.List))
+	}
+	return items, nil
+}
--- playlist/playlist.go
+++ playlist/playlist.go
@@ -3,9 +3,7 @@ package playlist
 import (
 	"fmt"
 	"time"
-	"strconv"
 	"log"
-	"errors"
 	yaml "github.com/kylelemons/go-gypsy/yaml"
 )
 
@@ -34,41 +32,4 @@ func (p *Playlist) Init(s time.Time, e time.Time, c yaml.File, xc yaml.File) *Pl
 		log.Fatalf("Invalid extras. %v", err)
 	}
  	return p
-}
-
-func validateItems(config yaml.File, key string) (yaml.List, error) {
-	node, err := yaml.Child( config.Root, key )
-	if err != nil {
-		return nil, err
-	}
-	lst, ok := node.(yaml.List)
-	if (!ok || (lst.Len() <= 0)) {
-		return nil, errors.New("Invalid items")
-	}
-	return lst, nil
-}
-
-func getItems(config yaml.File, key string) ([]*PlaylistBlock, error){
-	lst, err := validateItems(config, key)
-	if( err != nil){
-		return nil, errors.New("Invalid items")
-	}
-	items := make([]*PlaylistBlock, lst.Len())
-	for i, e := range lst {
-		itemKey := key + "[" + strconv.Itoa(i) + "]"
-		title, err := config.Get(itemKey + ".title")
-		if (err != nil) {
-			return nil, errors.New("Missing title")
-		}
-		series, err := config.Get(itemKey + ".series")
-		if (err != nil) {
-			series = ""
-		}
-		filepathsNode, err := yaml.Child( e, "filepaths" )
-		if err != nil {
-			return nil, errors.New("Missing filepaths for " + title)
-		}
-		items[i] = new(PlaylistBlock).Init(title, series, filepathsNode.(yaml.List))
-	}
-	return items, nil
 }
